7d3f0961d4229a319bc188a9b8d6be3976581641
Joni <joni.possi@gmail.com>
2021-03-18 00:10:53 +0200

    feat: Refactor context for new auth

--- components/AuthContext/AuthContext.js
+++ components/AuthContext/AuthContext.js
@@ -1,64 +1,10 @@
-import React, { createContext, useMemo } from 'react';
-import AsyncStorage from '@react-native-community/async-storage';
+import { createContext, useContext } from 'react';
+// import AsyncStorage from '@react-native-community/async-storage';
 
-const Context = createContext();
+const Context = createContext(null);
 
-function AuthContext({ children, dispatch }) {
-  const authContext = useMemo(
-    () => ({
-      signIn: async (data) => {
-        fetch('http://192.168.1.102:3000/user/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        })
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error('Error');
-            }
-            return response.json();
-          })
-          .then(async () => {
-            await AsyncStorage.setItem('userToken', JSON.stringify(data.token));
-            dispatch({ type: 'SIGN_IN', token: data.token });
-          })
-          .catch((error) => {
-            console.error('Error: ', error);
-          });
-      },
-      signOut: async () => {
-        await AsyncStorage.removeItem('userToken');
-        dispatch({ type: 'SIGN_OUT' });
-      },
-      signUp: async (data) => {
-        fetch('http://192.168.1.102:3000/user/register', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        })
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error('Error');
-            }
-            return response.json();
-          })
-          .then(async () => {
-            await AsyncStorage.setItem('userToken', JSON.stringify(data.token));
-            dispatch({ type: 'SIGN_IN', token: data.token });
-          })
-          .catch((error) => {
-            console.error('Error: ', error);
-          });
-      },
-    }),
-    [dispatch],
-  );
-
-  return <Context.Provider value={authContext}>{children}</Context.Provider>;
+function useAuthContext() {
+  return useContext(Context);
 }
 
-export { Context, AuthContext };
+export { Context, useAuthContext };
